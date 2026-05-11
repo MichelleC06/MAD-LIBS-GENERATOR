517043a221ce8312bea468ed5f90276418da9d01
MichelleC06 <mpc220000@utdallas.edu>
2026-05-11 11:53:07 -0500

    setting up window

---
+++ src/Madlib.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+from tkinter import *
+root = Tk()
+root.geometry('300x300')
+root.title('Madlib Generator')
+Label(root, text= "Madlib Generator \n 3 Fun Madlibs!", font = "arial 20 bold").pack()
+Label(root, text= "Choose your Madlib", font = "arial 15 bold").place(x=50, y=80)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
